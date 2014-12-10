8a383fd76ab57b64014c140cbfe29d7407f71789
krsola <kkk@kkk.pl>
2014-12-10 12:43:02 +0100

    initial version

--- app/cook-order-mgmt/js/cook-order-mgmt.module.js
+++ app/cook-order-mgmt/js/cook-order-mgmt.module.js
@@ -1,13 +1,13 @@
-angular.module('app.cook-order-mgmt', ['ngRoute', 'app.main', 'app.cook-order-mgmt.templates'], function ($routeProvider) {
+angular.module('app.cook-order-mgmt', ['ngRoute', 'app.main'], function ($routeProvider) {
     'use strict';
 //    translationSupportProvider.enableTranslationForModule('table-mgmt');
     $routeProvider.when('/cook-order-mgmt/order-view', {
         templateUrl: 'cook-order-mgmt/html/order-view.html',
         controller: 'CookOrderViewCntl',
         resolve: {
-            initialOrders: function () {
-                return [];
-            }
+            initialOrders: ['orders', function (orders) {
+                return orders.getOrdersPosition();
+            }]
         }
     });
 });
--- app/cook-order-mgmt/js/cook-search.controller.js
+++ app/cook-order-mgmt/js/cook-search.controller.js
@@ -1,6 +1,13 @@
 angular.module('app.cook-order-mgmt').controller('CookOrderViewCntl',
     function ($scope, initialOrders) {
         'use strict';
-        $scope.orders = initialOrders;
-
+        $scope.columnDefs = [
+            {field: 'orderId', label: 'Id'},
+            {field: 'offerName', label: 'Offer Name'}
+        ];
+        $scope.orderPositions = initialOrders;
+        $scope.assignOrder = function(orderPosition) {
+            // todo assign the orderPosition to staff member
+            orderPosition.state = 'ASSIGNED';
+        }
     });
