65858d165443741237aea5c02b472ca8fa3d7520
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2014-12-15 10:47:23 +0100

    table layout fixed

--- app/oasp-ui/js/oasp-grid/oasp-grid.module.js
+++ app/oasp-ui/js/oasp-grid/oasp-grid.module.js
@@ -1,1 +1,5 @@
-angular.module('oasp-ui.oasp-grid', ['app.oasp-ui.templates', 'trNgGrid']);
+/*global TrNgGrid*/
+angular.module('oasp-ui.oasp-grid', ['app.oasp-ui.templates', 'trNgGrid']).run(function () {
+    'use strict';
+    TrNgGrid.tableCssClass = "tr-ng-grid table table-striped";
+});
--- app/sales-mgmt/js/sales-mgmt.module.js
+++ app/sales-mgmt/js/sales-mgmt.module.js
@@ -1,5 +1,6 @@
 angular.module('app.sales-mgmt', ['app.main'])
     .config(function ($routeProvider) {
+        'use strict';
         $routeProvider.when('/sales-mgmt/cook-positions', {
             templateUrl: 'sales-mgmt/html/cook-positions.html',
             controller: 'CookPositionsCntl',
