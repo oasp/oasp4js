ffbdf3dec84a684dbf3d6d535c0da8bc26fdcfe9
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2014-12-16 19:32:28 +0100

    oasp 1.0.0

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+angular.module('app',
+    ['ui.select', 'ngRoute', 'app.main', 'app.table-mgmt', 'app.offer-mgmt', 'app.sales-mgmt'])
+    .config(function ($locationProvider, uiSelectConfig) {
+        'use strict';
+        $locationProvider.html5Mode(false);
+        uiSelectConfig.theme = 'bootstrap';
+    })
+    .run(function (globalSpinner) {
+        'use strict';
+        globalSpinner.showOnRouteChangeStartAndHideWhenComplete();
+    });
