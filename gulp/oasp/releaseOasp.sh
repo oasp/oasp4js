a3eb9df7e00b3008db8021ce6a378691f0694f6f
unknown <TBIALECK@CE09507.corp.capgemini.com>
2015-01-12 17:59:43 +0000

    1.0.1

---
+++ gulp/oasp/releaseOasp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "Please call ./releaseOasp versionToRelease"
+    exit
+fi
+cd ../../
+rm -rf ./dist
+git clone https://github.com/oasp/oasp4js.git dist
+cd dist
+shopt -s extglob dotglob
+rm -rf !(.git)
+cd ..
+gulp build:oasp
+git commit -am "$1"
